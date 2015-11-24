35792d341532574d6135b69844bfa1ea7ac3301d
Philippine Ramaherison <ramahe_p@epitech.eu>
2015-11-24 12:25:36 +0000

    preparation of classes First fill of ChessApplication and CustomControl classes

--- src/ChessApplication/CustomControl.java
+++ src/ChessApplication/CustomControl.java
@@ -10,5 +10,44 @@ public class CustomControl extends Control {
 	
 	//similar to previous custom controlls but must handle more
 	//complex mouse interactions and key interactions
+	
+	// constructor
+	public CustomControl() {
+		// set a default skin, generate a board and add the board to the control
+		setSkin(new CustomControlSkin(this));
+		
+		chessBoard = new ChessBoard();
+		
+		getChildren().add(chessBoard);
+
+		// mouse clicked event handler that will try to place a piece on the board
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				chessBoard.selectPiece(event.getX());
+				chessBoard.movePiece();
+			}
+		});
+	
+		// key stroke event handler which will call the restGame method when the space bar is pressed
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.SPACE)
+					chessBoard.resetGame();
+			}
+		});
+	}
+
+	//override the resize method
+	@Override
+	public void resize(double width, double height) {
+		// call the super class method and update the size of the board
+		super.resize(width, height);
+		chessBoard.resize(width, height);
+	}
+
+	//private fields of the class
+	private ChessBoard chessBoard;	// a Chess board
 
 }
