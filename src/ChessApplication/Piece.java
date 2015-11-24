12069f2a6217c5f853f9ec64119f071dd6d81f9f
plassa-b <clery1.plassat@epitech.eu>
2015-11-24 13:16:35 +0000

    Modifs on Piece and Pawn Add method getMoves to get a list of possible moves for the piece. Implemented in PiecePawn.

--- src/ChessApplication/Piece.java
+++ src/ChessApplication/Piece.java
@@ -1,5 +1,9 @@
 package ChessApplication;
 
+import java.util.List;
+
+import com.sun.javafx.geom.Vec2d;
+
 import javafx.scene.image.Image;
 
 //class declaration - abstract because we will not want to create a Piece object but we would
@@ -7,21 +11,23 @@ import javafx.scene.image.Image;
 public abstract class Piece {
 	
 	//piece can be either white (1) or black (2)
-	private int type; 
+	private int type;
 	//image used to display the piece
 	private Image image;
 	
-	public Piece(int type){
-		this.type= type;
+	public static final int WHITE = 1;
+	public static final int BLACK = 2;
+
+	public Piece(int type) {
+		this.type = type;
+	}
+	
+	public int GetType() {
+		return (type);
 	}
 	
 	//move method
-	public void movePiece(double x, double y) {
-		
-	}
-	
-	//capture method
-	public void getCaptured() {
-		
-	}
+//	abstract public boolean movePiece(double x, double y);
+
+	abstract public List<Vec2d> getMoves(int x, int y, Piece[][] board);
 }
--- src/ChessApplication/PiecePawn.java
+++ src/ChessApplication/PiecePawn.java
@@ -1,5 +1,41 @@
 package ChessApplication;
 
-public class PiecePawn extends Piece{
+import java.util.ArrayList;
+import java.util.List;
 
+import com.sun.javafx.geom.Vec2d;
+
+public class PiecePawn extends Piece{
+	public PiecePawn(int type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Vec2d> getMoves(int x, int y, Piece[][] board) {
+		List<Vec2d> moves = new ArrayList<Vec2d>();
+		
+		if (GetType() == BLACK)	{
+			if (y < 7)
+			{
+				if (board[x][y + 1] == null)
+					moves.add(new Vec2d(x, y + 1));
+				if (x > 0 && board[x - 1][y + 1] != null && board[x - 1][y + 1].GetType() == WHITE)
+					moves.add(new Vec2d(x - 1, y + 1));
+				if (x < 7 && board[x + 1][y + 1] != null && board[x + 1][y + 1].GetType() == WHITE)
+					moves.add(new Vec2d(x + 1, y + 1));
+			}
+		}
+		else {
+			if (y > 0)
+			{
+				if (board[x][y - 1] == null)
+					moves.add(new Vec2d(x, y - 1));
+				if (x > 0 && board[x - 1][y - 1] != null && board[x - 1][y - 1].GetType() == BLACK)
+					moves.add(new Vec2d(x - 1, y - 1));
+				if (x < 7 && board[x + 1][y - 1] != null && board[x + 1][y - 1].GetType() == BLACK)
+					moves.add(new Vec2d(x + 1, y - 1));
+			}
+		}
+		return (moves);
+	}
 }
