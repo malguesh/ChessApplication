3e877d237f8577a9b1796359d3457d04d286346b
plassa-b <clery1.plassat@epitech.eu>
2015-11-24 12:25:50 +0000

    ChessBoard basics Add main methods, some code for resize and reset.

--- src/ChessApplication/ChessBoard.java
+++ src/ChessApplication/ChessBoard.java
@@ -1,18 +1,79 @@
 package ChessApplication;
 
-public class ChessBoard {
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class ChessBoard extends Pane {
 	
 	public ChessBoard() {
 		//initalize the board: background, data structures, inital layout of pieces
+		
 	}
 	
 	//resize method
+	@Override
+	public void resize(double width, double height) {
+		//call the superclass resize method  
+		super.resize(width, height);
+
+		// resize the rectangle to take the full size of the widget 
+		background.setWidth(width);
+		background.setHeight(height);
+		
+		// calculate the width and height of a cell in which a windows and a piece will sit		 
+		cell_width = width / (float)boardWidth;
+		cell_height = height / (float)boardHeight;
+
+		// nested for loop to reset the sizes and positions of all pieces that were already placed 
+		// and update the position of the windows in the board 
+		for (int i = 0; i < boardWidth; ++i)
+			for (int j = 0; j < boardHeight; ++j)
+			{
+				if (pieces[i][j] != null)
+				{
+//					pieces[i][j].relocate(i * cell_width, j * cell_height);
+//					pieces[i][j].resize(cell_width, cell_height);
+				}
+			}
+	}
 	
 	//reset game method
+	public void resetGame() {
+		for (int i = 0; i < boardWidth; ++i)
+			for (int j = 0; j < boardHeight; ++j)
+			{
+				board[i][j] = EMPTY;
+				getChildren().remove(pieces[i][j]);
+				pieces[i][j] = null;
+			}
+		current_player = PlayerWhite;
+	}
 	
 	//select piece method
+	public void selectPiece(int x, int y) {
+		
+	}
 	
 	//move piece method
+	public void movePiece(int x, int y) {
+		
+	}
 	
 	//private fields
+	private int boardWidth = 8;			// the width of the Connect4 board
+	private int boardHeight = 8;		// the height of the Connect4 board
+	private int[][] board; 				// 2D array that holds  int values representing the pieces
+	private Piece[][] pieces; 			// 2D array that holds the renders of the pieces
+	private Rectangle background; 		// background of the board
+	private double cell_width;			// width of a cell in the board
+	private double cell_height; 		// height of a cell in the board
+	private int current_player; 		// hold the value of the current player (PlayerRed or Player Yellow) 
+
+	// constants to be inserted into the 2D board array to keep track of the location of cells containing 
+	// empty, white and black pieces 
+	private final int EMPTY = 0;		// 0 is used to indicate that a cell in the board is unoccupied
+	private final int PlayerWhite = 1;	// 1 is used to indicate that a cell in the board is occupied by a red piece
+	private final int PlayerBlack = 2; // 2 is used to indicate that a cell in the board is occupied by a yellow piece
+	
+	int winner = 0;						// variable to determine who the current winner is: 0 - no current winner, 
 }
